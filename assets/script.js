fb4d891fc3763ae9ac818321a515bb4f70b994c4
Ross F <rfahan@gmail.com>
2023-07-06 20:53:52 -0600

    added some starter code for question objects

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//objects for questions
+var questions = [
+    {
+      question: "Question 1",
+      options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      answer: 0
+    },
+    {
+      question: "Question 2",
+      options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      answer: 1
+    },
+  ];
