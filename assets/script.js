bde7ff0d074ea6607cdfd5a246490867c6b32b22
Ross F <rfahan@gmail.com>
2023-08-23 20:50:26 -0600

    added functionality to display proper elements at correct times

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,10 @@
-//global variables
-var questionIndex = 0;
+// Global variables
+var currentIndex = 0;
 var timer = 60;
+var score = 0;
+var interval;
 
-//objects for questions
-var questions = [
-    {
-      question: "Question 1",
-      options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-      answer: "option 1"
-    },
-    {
-      question: "Question 2",
-      options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-      answer: 1
-    },
-  ];
-
-
-  //event listeners
-
-
-  //DOM
+// DOM elements
 var startScreen = document.getElementById('start-screen');
 var startButton = document.getElementById('start-btn');
 var quizSection = document.getElementById('quiz');
@@ -29,53 +13,94 @@ var optionsElement = document.getElementById('options');
 var timerElement = document.getElementById('timer');
 var endGameSection = document.getElementById('end-game');
 var finalScoreElement = document.getElementById('final-score');
-var initialsInput = document.getElementById('initials');
-var submitButton = document.getElementById('submit-btn');
 
-var currentIndex = 0;
+// Questions data
+var questions = [
+  {
+    question: "Question 1",
+    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    answer: "Option 1"
+  },
+  {
+    question: "Question 2",
+    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    answer: "Option 2"
+  },
+  // ... Add more questions here
+];
 
-  //quiz start function
-var questionTag = document.createElement("h1");
-questionTag.textContent = questions[currentIndex].question
-questionElement.appendChild(questionTag);
+// Event listener for start button
+startButton.addEventListener('click', function () {
+  // Hide the start-screen section
+  startScreen.style.display = 'none';
+  // Display questions and options
+  displayQuestionAndOptions();
+  // Start timer
+  startTimer();
+});
 
-for(let i=0; i < questions[currentIndex].options.length; i++){
-    var button = document.createElement("button");
-    button.setAttribute("class", "options-btn")
-    button.innerHTML = questions[0].options[i];
+// Function to display questions and options
+function displayQuestionAndOptions() {
+  // Show the quiz section
+  quizSection.style.display = 'block';
+  // Hide the end-game section
+  endGameSection.style.display = 'none';
+
+  // Display the question
+  questionElement.textContent = questions[currentIndex].question;
+
+  // Clear previous options
+  optionsElement.innerHTML = '';
+
+  // Loop through options and create buttons
+  for (let i = 0; i < questions[currentIndex].options.length; i++) {
+    var button = document.createElement('button');
+    button.setAttribute('class', 'options-btn');
+    button.textContent = questions[currentIndex].options[i];
     button.onclick = displayOptions;
     optionsElement.appendChild(button);
+  }
 }
 
-// Initially, hide the quiz and end-game sections
-quizSection.style.display = 'none';
+// Display options
+function displayOptions(event) {
+  var selectedOption = event.target.textContent;
+  // Check if selectedOption matches the correct answer
+  if (selectedOption === questions[currentIndex].answer) {
+    score++;
+  }
+  currentIndex++;
+  if (currentIndex < questions.length) {
+    displayQuestionAndOptions();
+  } else {
+    endGame();
+  }
+}
+
+// Function to start the timer
+function startTimer() {
+  interval = setInterval(function () {
+    timerElement.textContent = 'Time: ' + timer;
+    timer--;
+
+    // Stop the timer when it reaches 0
+    if (timer < 0) {
+      clearInterval(interval);
+      endGame();
+    }
+  }, 1000);
+}
+
+// Initially, hide the end-game section
 endGameSection.style.display = 'none';
 
-function displayOptions(event){
-  var element = event.target.innerHTML;
-  console.log(element);
-
+// Function to handle end of game
+function endGame() {
+  clearInterval(interval);
+  // Hide the quiz section
+  quizSection.style.display = 'none';
+  // Display the end-game section
+  endGameSection.style.display = 'block';
+  // Display final score
+  finalScoreElement.textContent = 'Your final score is: ' + score;
 }
-
-  //check correct
-
-
-  //timer
-  function startTimer() {
-    var interval = setInterval(function () {
-      timerElement.textContent = 'Time: ' + timer;
-      timer--;
-  
-      // Stop the timer when it reaches 0
-      if (timer < 0) {
-        clearInterval(interval);
-        endGame();
-      }
-    }, 1000);
-  }
-  
-
-  //end game
-
-
-  //high score
