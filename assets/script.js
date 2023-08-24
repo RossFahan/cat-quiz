15400a6782f43d68b0e7e397872a3d7c43cf34e8
Ross F <rfahan@gmail.com>
2023-08-23 20:24:33 -0600

    added timer

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ var questions = [
     {
       question: "Question 1",
       options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-      answer: 0
+      answer: "option 1"
     },
     {
       question: "Question 2",
@@ -32,15 +32,48 @@ var finalScoreElement = document.getElementById('final-score');
 var initialsInput = document.getElementById('initials');
 var submitButton = document.getElementById('submit-btn');
 
+var currentIndex = 0;
 
   //quiz start function
+var questionTag = document.createElement("h1");
+questionTag.textContent = questions[currentIndex].question
+questionElement.appendChild(questionTag);
 
+for(let i=0; i < questions[currentIndex].options.length; i++){
+    var button = document.createElement("button");
+    button.setAttribute("class", "options-btn")
+    button.innerHTML = questions[0].options[i];
+    button.onclick = displayOptions;
+    optionsElement.appendChild(button);
+}
+
+// Initially, hide the quiz and end-game sections
+quizSection.style.display = 'none';
+endGameSection.style.display = 'none';
+
+function displayOptions(event){
+  var element = event.target.innerHTML;
+  console.log(element);
+
+}
 
   //check correct
 
 
   //timer
-
+  function startTimer() {
+    var interval = setInterval(function () {
+      timerElement.textContent = 'Time: ' + timer;
+      timer--;
+  
+      // Stop the timer when it reaches 0
+      if (timer < 0) {
+        clearInterval(interval);
+        endGame();
+      }
+    }, 1000);
+  }
+  
 
   //end game
 
